439c741a7a1503273ed65b93955746f5b9655254
micahke <micahkelias@gmail.com>
2025-08-08 17:27:32 -0700

    updated mocks

--- mocks/clients/cache/mock_cache.go
+++ mocks/clients/cache/mock_cache.go
@@ -169,3 +169,17 @@ func (mr *MockCacheMockRecorder) Set(arg0, arg1, arg2, arg3 any) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Set", reflect.TypeOf((*MockCache)(nil).Set), arg0, arg1, arg2, arg3)
 }
+
+// SetMany mocks base method.
+func (m *MockCache) SetMany(arg0 context.Context, arg1 []string, arg2 []any, arg3 time.Duration) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SetMany", arg0, arg1, arg2, arg3)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// SetMany indicates an expected call of SetMany.
+func (mr *MockCacheMockRecorder) SetMany(arg0, arg1, arg2, arg3 any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SetMany", reflect.TypeOf((*MockCache)(nil).SetMany), arg0, arg1, arg2, arg3)
+}
